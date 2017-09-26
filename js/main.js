3948ec9ee91e90beed6df02e585a978f396ebeee
Beatriz Yumi Simões de Castro <30733870+beatrizyumi@users.noreply.github.com>
2017-09-25 22:09:58 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,38 +1,84 @@
-var saldo = parseFloat(document.querySelector('.saldo').innerText);
-var valor = parseFloat(document.querySelector('.quantia').value);
+$(document).ready(function() {
 
-function retirada() {
-    if (isNaN(parseFloat(document.querySelector('.quantia').value))) {
-        alert('Valor nao valido, incluir um numero');
-        valor=0;
-    } else { valor = parseFloat(document.querySelector('.quantia').value)};
-	if (valor > saldo) {
-        console.log('Nao e permitido');
-        console.log('Seu saldo e ' + saldo);
-    } else if (valor == saldo) {
-        console.log('Permitido, POBRE!');
-        saldo = saldo - valor;
-        document.querySelector('.saldo').innerText = saldo;
-        console.log('Seu saldo e ' + saldo);
-    } else {
-        console.log('Permitido');
-        saldo = saldo - valor;
-        document.querySelector('.saldo').innerText = saldo;
-        console.log('Seu saldo e ' + saldo);
+    var saldo = 100;
+    var saldoPoupanca = 0;
+    var valor;
+
+    $('.saldo').text(saldo.toLocaleString('currency', {style: 'currency', currency: 'BRL'}));
+    $('.saldo-poupanca').text(saldoPoupanca.toLocaleString('currency', {style: 'currency', currency: 'BRL'}));
+
+    function trocaClasse(elemento, adicionar, retirar) {
+        $(elemento).addClass(adicionar);
+        $(elemento).removeClass(retirar);
     }
-}
 
-function deposito() {
-    if (isNaN(parseFloat(document.querySelector('.quantia').value))) {
-        alert('Valor nao valido, incluir um numero');
-        valor=0;
-    } else { valor = parseFloat(document.querySelector('.quantia').value)};
-	saldo = saldo + valor;
-    document.querySelector('.saldo').innerText = saldo;
-	console.log('Deposito realizado com sucesso');
-	console.log('Seu saldo atual e de ' + saldo);
-}
+    function validaErro() {
+        if (isNaN(parseFloat($('.quantia').val()))) {
+            alert('Valor invalido. Incluir apenas numeros');
+            valor = 0;
+        } else {
+            valor = parseFloat($('.quantia').val())
+        }
+    }
 
-document.querySelector('.depositar').addEventListener('click', deposito);
+    function trocaSaldo() {
+        $('.saldo').text(saldo.toLocaleString('currency', {style: 'currency', currency: 'BRL'}));
+        $('.historico').append('<li>' + saldo.toLocaleString('currency', {style: 'currency', currency: 'BRL'}) + '</li>');        
+    }
 
-document.querySelector('.sacar').addEventListener('click', retirada);
+    function trocaSaldoPoupanca() {
+        $('.saldo-poupanca').text(saldoPoupanca.toLocaleString('currency', {style: 'currency', currency: 'BRL'}));
+        $('.historico-poupanca').append('<li>' + saldoPoupanca.toLocaleString('currency', {style: 'currency', currency: 'BRL'}) + '</li>');
+    }
+
+    $('.depositar').on('click', function() {
+        validaErro();
+        var botao = $(this).text().match('Poup')
+        console.log($(this));
+        console.log(botao);
+        if (botao != null && botao.length == 1){
+            saldoPoupanca = saldoPoupanca + valor;
+            trocaClasse('.saldo-poupanca', 'verde', 'vermelho');
+            trocaSaldoPoupanca();
+        } else {
+            saldo = saldo + valor;
+            trocaClasse('.saldo', 'verde', 'vermelho');
+            trocaSaldo();
+        };
+    });
+
+    $('.sacar').on('click', function() {
+        validaErro();
+        var botao = $(this).text().match('Poup')
+        console.log($(this));
+        console.log(botao);
+        if (botao != null && botao.length == 1) {
+            if (saldoPoupanca < valor) {
+                alert('Operação indevida, saldo insuficiente');
+            } else if (saldoPoupanca == valor) {
+                saldoPoupanca = saldoPoupanca - valor;
+                trocaClasse('.saldo-poupanca', 'vermelho', 'verde');
+                trocaSaldoPoupanca();
+                alert('Voce está pobre :(');
+            } else {
+                saldoPoupanca = saldoPoupanca - valor;
+                trocaClasse('.saldo-poupanca', 'vermelho', 'verde');
+                trocaSaldoPoupanca();
+            };
+        }
+        else {
+            if(saldo < valor) {
+                alert('Operação indevida, saldo insuficiente');
+            } else if (saldo == valor) {
+                saldo = saldo - valor;
+                alert('Voce está pobre :(');
+                trocaClasse('.saldo', 'vermelho', 'verde');
+                trocaSaldo();
+            } else {
+                saldo = saldo - valor;
+                trocaClasse('.saldo', 'vermelho', 'verde');
+                trocaSaldo();
+            };
+        };       
+    });
+});
